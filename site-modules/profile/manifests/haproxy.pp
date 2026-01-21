1ade43da8cf3ad322586a32f69c440cdaca6665e
Ubuntu <ubuntu@ip-10-0-9-11.eu-west-1.compute.internal>
2026-01-21 16:50:25 +0000

    haproxy: use ['networking']['ip']

--- site-modules/profile/manifests/haproxy.pp
+++ site-modules/profile/manifests/haproxy.pp
@@ -5,7 +5,7 @@ class profile::haproxy {
 
   haproxy::listen { 'puppet00':
     collect_exported => false,
-    ipaddress        => $facts['ec2_metadata']['public-ipv4'],
+    ipaddress        => $facts['networking']['ip'], # $facts['ec2_metadata']['public-ipv4'],
     ports            => [8140],
   }
   haproxy::balancermember { 'server00':
