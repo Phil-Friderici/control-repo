412ef570d932679bee86d90db69687067cab9448
Ubuntu <ubuntu@ip-10-0-9-11.eu-west-1.compute.internal>
2026-01-22 10:51:28 +0000

    Use only compiler5

--- site-modules/profile/manifests/haproxy.pp
+++ site-modules/profile/manifests/haproxy.pp
@@ -8,24 +8,32 @@ class profile::haproxy {
     ipaddress        => $facts['networking']['ip'], # $facts['ec2_metadata']['public-ipv4'],
     ports            => [8140],
   }
-  haproxy::balancermember { 'server00':
+#  haproxy::balancermember { 'server00':
+#    listening_service => 'puppet00',
+#    server_names      => 'ec2-108-129-144-12.eu-west-1.compute.amazonaws.com',
+#    ipaddresses       => '108.129.144.12',
+#    ports             => [8140],
+#    options           => 'check',
+#  }
+#  haproxy::balancermember { 'server01':
+#    listening_service => 'puppet00',
+#    server_names      => 'ec2-34-241-7-165.eu-west-1.compute.amazonaws.com',
+#    ipaddresses       => '34.241.7.165',
+#    ports             => [8140],
+#    options           => 'check',
+#  }
+#  haproxy::balancermember { 'server02':
+#    listening_service => 'puppet00',
+#    server_names      => 'ec2-3-148-241-213.us-east-2.compute.amazonaws.com',
+#    ipaddresses       => '3.148.241.213',
+#    ports             => [8140],
+#    options           => 'check',
+#  }
+
+  haproxy::balancermember { 'server05':
     listening_service => 'puppet00',
-    server_names      => 'ec2-108-129-144-12.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '108.129.144.12',
-    ports             => [8140],
-    options           => 'check',
-  }
-  haproxy::balancermember { 'server01':
-    listening_service => 'puppet00',
-    server_names      => 'ec2-34-241-7-165.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '34.241.7.165',
-    ports             => [8140],
-    options           => 'check',
-  }
-  haproxy::balancermember { 'server02':
-    listening_service => 'puppet00',
-    server_names      => 'ec2-3-148-241-213.us-east-2.compute.amazonaws.com',
-    ipaddresses       => '3.148.241.213',
+    server_names      => 'ec2-34-245-138-203.eu-west-1.compute.amazonaws.com',
+    ipaddresses       => '34.245.138.203',
     ports             => [8140],
     options           => 'check',
   }
