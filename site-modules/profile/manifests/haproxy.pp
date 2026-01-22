788f48924ee396cfab98afc6cbe0b4af3c65d9b5
Ubuntu <ubuntu@ip-10-0-9-11.eu-west-1.compute.internal>
2026-01-22 12:26:02 +0000

    Back to compiler5

--- site-modules/profile/manifests/haproxy.pp
+++ site-modules/profile/manifests/haproxy.pp
@@ -8,13 +8,13 @@ class profile::haproxy {
     ipaddress        => $facts['networking']['ip'], # $facts['ec2_metadata']['public-ipv4'],
     ports            => [8140],
   }
-  haproxy::balancermember { 'server00':
-    listening_service => 'puppet00',
-    server_names      => 'ec2-108-129-144-12.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '108.129.144.12',
-    ports             => [8140],
-    options           => 'check',
-  }
+#  haproxy::balancermember { 'server00':
+#    listening_service => 'puppet00',
+#    server_names      => 'ec2-108-129-144-12.eu-west-1.compute.amazonaws.com',
+#    ipaddresses       => '108.129.144.12',
+#    ports             => [8140],
+#    options           => 'check',
+#  }
 #  haproxy::balancermember { 'server01':
 #    listening_service => 'puppet00',
 #    server_names      => 'ec2-34-241-7-165.eu-west-1.compute.amazonaws.com',
