311887482cbec9329553c73bce3d580f2fce5974
Ubuntu <ubuntu@ip-10-0-9-11.eu-west-1.compute.internal>
2026-01-22 10:03:12 +0000

    Add PXP port to haproxy

--- site-modules/profile/manifests/haproxy.pp
+++ site-modules/profile/manifests/haproxy.pp
@@ -6,20 +6,20 @@ class profile::haproxy {
   haproxy::listen { 'puppet00':
     collect_exported => false,
     ipaddress        => $facts['networking']['ip'], # $facts['ec2_metadata']['public-ipv4'],
-    ports            => [8140],
+    ports            => [8140,8142],
   }
   haproxy::balancermember { 'server00':
     listening_service => 'puppet00',
     server_names      => 'ec2-108-129-144-12.eu-west-1.compute.amazonaws.com',
     ipaddresses       => '108.129.144.12',
-    ports             => [8140],
+    ports             => [8140,8142],
     options           => 'check',
   }
   haproxy::balancermember { 'server01':
     listening_service => 'puppet00',
     server_names      => 'ec2-34-241-7-165.eu-west-1.compute.amazonaws.com',
     ipaddresses       => '34.241.7.165',
-    ports             => [8140],
+    ports             => [8140,8142],
     options           => 'check',
   }
 
@@ -27,7 +27,7 @@ class profile::haproxy {
     listening_service => 'puppet00',
     server_names      => 'ec2-3-148-241-213.us-east-2.compute.amazonaws.com',
     ipaddresses       => '3.148.241.213',
-    ports             => [8140],
+    ports             => [8140,8142],
     options           => 'check',
   }
 }
